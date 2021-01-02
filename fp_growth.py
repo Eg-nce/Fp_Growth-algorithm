62bcaf1f75b37da187f408e19912dfdec7782e45
Eg-nce <69755600+Eg-nce@users.noreply.github.com>
2021-01-02 07:38:17 +0300

    Update fp_growth.py

--- fp_growth.py
+++ fp_growth.py
@@ -54,8 +54,8 @@ class Fp_Growth(General_Tree):
 
     def __MinsupKiller(self, Data ,Minsup):
   
-        s1 = {}
-        s2 = {} 
+        count_dic = {}
+        clean_dic = {} 
         
         try:
            Data =  Data.applymap(str.lower)
@@ -63,33 +63,34 @@ class Fp_Growth(General_Tree):
             TypeError
         Data =  Data.fillna('NaNxx')
         Data =  Data.values.tolist()
-        i = 0 
+        row = 0 
         while True:
             try:    
-                Data[i].remove("NaNxx")
+                Data[row].remove("NaNxx")
             except :   
                 ValueError
-                i = i + 1 
-            if i == len(Data):
+                row = row + 1 
+            if row == len(Data):
                 break  
             
-        for i in Data:
-            for j in i:
-                if j in s1:
-                    s1[j] += 1
+        for row in Data:
+            for item in row:
+                if item in count_dic:
+                    count_dic[item] += 1
                 else:
-                    s1[j] = 1
-        for x, y in s1.items():
-            if y >= self.Minsup:
-                s2[x] = y
+                    count_dic[item] = 1
+        
+        for item, number in count_dic.items():
+            if number >= self.Minsup:
+                 clean_dic[item] = number
             else:
                 for elements in Data:
-                    if x in elements:
-                        elements.remove(x)
+                    if item in elements:
+                        elements.remove(item)
 
         Data = sorted(Data, key=lambda x: len(x), reverse=True)
         Data = list(filter(None, Data))
-        Label = {k: v for k, v in sorted(s2.items(), reverse=True, key=lambda item: item[1])}
+        Label = {k: v for k, v in sorted( clean_dic.items(), reverse=True, key=lambda item: item[1])}
         if len(Label) == 0:
               raise TypeError("minimum support candidate and confidences does not exist !!!\n try decrease minsup or confidence rate")
       
@@ -99,43 +100,46 @@ class Fp_Growth(General_Tree):
     def __Sorter(self):
         self.CleanData  = self.__MinsupKiller(self.Data,self.Minsup)
         Sort_list = []
-        z = 0
+        row = 0
         sort_based = [x for x in  self.Label.keys()]
         while True:
 
             for labels in sort_based:
 
-                if labels in self.CleanData[z]:
+                if labels in self.CleanData[row]:
                     Sort_list.append(labels)
 
-            self.CleanData[z] = Sort_list
+            self.CleanData[row] = Sort_list
+
             Sort_list = []
-            z = z + 1
-            if z == len(self.CleanData):
+
+            row = row + 1
+            if row == len(self.CleanData):
                 break
         return self.CleanData
 
     def __empty(self,num):
-        L = []
+        empty_list = []
         for x in range(num):
-            L.append([])
-        return L
+            empty_list.append([])
+        return empty_list
 
     def __Lister(self,Data):
-        L = self.__empty(len([x for x in self.Label.keys()]))
+        empty_lists = self.__empty(len([x for x in self.Label.keys()]))
         indexs = [x for x in self.Label.keys()]
         try:
-            for z in Data:
-                var = indexs.index(z[0])
-                L[var].append(z)
+            for elem in Data:
+                var = indexs.index(elem[0])
+                empty_lists[var].append(elem)
         except:
             IndexError
-        L = list(filter(None, L))
-        return L
+        empty_lists = list(filter(None, empty_lists))
+        return empty_lists
 
     def __MakeRoot(self):
         Root = General_Tree("Root")
         return Root
+
 
     def __mainroot(self,M):
         Tree_list = []
@@ -154,20 +158,20 @@ class Fp_Growth(General_Tree):
     def __Create_and_Count(self):
         Main  =  self.__MakeRoot()
         Pre_list = self.__Sorter()
-        List = self.__Lister( Pre_list)
-        rtx = 0
+        List = self.__Lister(Pre_list)
+        item = 0
 
         while True:
-            Leaf = self.__mainroot(List[rtx][0])
-            List[rtx].pop(0)
+            Leaf = self.__mainroot(List[item][0])
+            List[item].pop(0)
             Main.New_Item(Leaf)
-            rtx = rtx + 1
-            if rtx == len(List):
+            item = item + 1
+            if item == len(List):
                 break
         Main_Back = Main
         List =  list(filter(None, List) )
 
-        x = 0;  y = 0; c = 0
+        root = 0;  leaf = 0; elem = 0
 
         while True:
             
@@ -175,7 +179,7 @@ class Fp_Growth(General_Tree):
             try:
                 for elem in Main.Get_childrens():
 
-                    if (List[x][y][c] in elem) == True:
+                    if (List[root][leaf][item] in elem) == True:
                         index_list.append("Y")
 
 
@@ -183,37 +187,37 @@ class Fp_Growth(General_Tree):
                         index_list.append("N")
 
                 if "Y" not in index_list:
-                    Leaf = self.__mainroot(List[x][y][List[x][y].index(List[x][y][c]):])
+                    Leaf = self.__mainroot(List[root][leaf][ List[root][leaf].index(List[root][leaf][item]):])
                     Main.New_Item(Leaf)
-                    c = 0
-                    y = y + 1
+                    item= 0
+                    leaf = leaf + 1
                     Main = Main_Back
                 else:
                     index = index_list.index("Y")
                     Main = Main.GetChild(index)
                     Main.AddPlus()
-                    c = c + 1
-                    if c == len(List[x][y]):
-                        y = y + 1
-                        c = 0
+                    item = item + 1
+                    if item == len(List[root][leaf]):
+                        leaf = leaf + 1
+                        item = 0
                         Main = Main_Back
-                if y == len(List[x]):
-                    y = 0
-                    c = 0
-                    x = x + 1
+                if leaf == len(List[root]):
+                    leaf = 0
+                    item = 0
+                    root = root + 1
             except IndexError:
                 Main = Main_Back
-                break      
-
+                break
       
-        l = 0
+      
+        step = 0
         counted_list = [] 
         while True:
             try:
 
                 Main = Main.GetChild(0)
                 counted_list.append([])
-                counted_list[l].append(Main.GetVal())
+                counted_list[step].append(Main.GetVal())
             except IndexError:
 
                 while True:
@@ -223,7 +227,7 @@ class Fp_Growth(General_Tree):
                             
                             Main = Main_Back
                             Main.Kill_Children(0)
-                            l = l + 1
+                            step = step + 1
                             break 
                     else:    
                         Main.Kill_Children(0)
@@ -234,7 +238,7 @@ class Fp_Growth(General_Tree):
                             if len(Main.Get_Parent().Get_childrens()) > 1:
                                 Main = Main.Get_Parent()
                                 Main.Kill_Children(0)
-                                l = l + 1
+                                step = step + 1
                                 Main = Main_Back
                                 break
                         except IndexError:
@@ -247,31 +251,34 @@ class Fp_Growth(General_Tree):
     
     
     def __Ready_list(self):
-        def co(elem):
+        def couter(elem):
             var = elem.count("+")
             yield var
         
-        def key2(List):
+        def replacer(List):
             var = List.replace("+" , "")
             yield var
         
         counted = self.__Create_and_Count()
-        final_list = []; j = 0; i = 0 ;  z = {}
+        final_list = []; List = 0; item = 0 ;  counted_dic = {}
         
         while True:
-            var = next(co(counted[j][i]))
-            key = counted[j][i]
-            key  = next(key2(key))
-            z[key] = var + 1
-            i = i + 1
+           
+            var = next(couter(counted[List][item]))
+            key = counted[List][item]
+            key = next(replacer(key))
+           
 
-            if i == len(counted[j]):
-                z = {z1: d for z1, d in sorted(z.items(), reverse=True, key=lambda item: item[1])}
-                final_list.append(z)
-                z = {}
-                j = j + 1
-                i = 0
-            if j == len(counted):
+            counted_dic[key] = var + 1
+            item = item + 1
+
+            if item == len(counted[List]):
+                counted_dic  = {z1: d for z1, d in sorted(counted_dic.items(), reverse=True, key=lambda item: item[1])}
+                final_list.append(counted_dic )
+                counted_dic  = {}
+                List = List + 1
+                item = 0
+            if List == len(counted):
                 break
 
         return  final_list
@@ -289,29 +296,30 @@ class Fp_Growth(General_Tree):
          val= 0
          final_list =   [i for n, i in enumerate( final_list) if i not in  final_list[n + 1:]]
   
-         for z in  final_list:
-           if ( Labels[-1] in z) == True:
-             index_list.append(final_list.index(z))
-             val_list.append(z.get(Labels[-1]))
-             z.pop(Labels[-1])
-         for x in index_list:
-            L =  final_list[x].copy()
+         for part in  final_list:
+           if ( Labels[-1] in part) == True:
+             index_list.append(final_list.index(part))
+             val_list.append(part.get(Labels[-1]))
+             part.pop(Labels[-1])
+         
+         for index in index_list:
+            process_list =  final_list[index].copy()
 
-            for com in range(len(L)+1):
-                Key_cmb = combinations(L.keys(),com+1)
-                for j in Key_cmb:
+            for com in range(len(process_list)+1):
+                Key_cmb = combinations(process_list.keys(),com+1)
+                for key in Key_cmb:
                     temp  = []
-                    j = list(j)
-                    j.append( Labels[-1])
+                    key = list(key)
+                    key.append( Labels[-1])
           
-                    if (j not in Seen_list) == True:
-                        Seen_list.append(j)
-                        temp.append(j)
+                    if (key not in Seen_list) == True:
+                        Seen_list.append(key)
+                        temp.append(key)
                         temp.append("======>")
                         temp.append(val_list[val])
                         Fp_list.append(temp)
                     else:
-                        var = Seen_list.index(j)
+                        var = Seen_list.index(key)
                         var = var + sizeof
                         Fp_list[var][2] = Fp_list[var][2] + val_list[val]
       
@@ -324,10 +332,10 @@ class Fp_Growth(General_Tree):
            break
       
       
-      for gtx  in  Fp_list:
-        if gtx[2] >= self.Minsup:
+      for Fp_item  in  Fp_list:
+        if Fp_item[2] >= self.Minsup:
          
-             Clean_list.append(gtx)
+             Clean_list.append(Fp_item)
       
       for lab , val  in self.Label.items():
           temp_inside = []
@@ -344,6 +352,7 @@ class Fp_Growth(General_Tree):
       
       return tuple(Clean_list)
       
+
     def Pickaxe(self):
         M_list =  self.__minsups()
         if len(M_list) == 1:
@@ -370,12 +379,13 @@ class Fp_Growth(General_Tree):
                         confidence_temp.append( "confidence =  % {}".format(confidence_var))
                         Confidence.append( confidence_temp)
 
+
             queue = queue + 1      
             if queue == len(Con_list):
                 break
         
-        for x in Pickaxe:
-            x[2]= ( "% {} ".format((x[2]*100)/len(self.Data)))
+        for Pic_item in Pickaxe:
+            Pic_item[2]= ( "% {} ".format((Pic_item[2]*100)/len(self.Data)))
 
         return Pickaxe ,Confidence 
 
